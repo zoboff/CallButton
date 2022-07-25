6ebc2084347b66faa41ac0224ff3e1d3ccd5e32d
Andrey Zobov <zoboff@gmail.com>
2022-07-25 16:12:03 +0300

    init

---
+++ main.py
@@ -0,0 +1,151 @@
+﻿import sys
+import pyVideoSDK
+from pyVideoSDK.methods import Methods
+from pyVideoSDK.consts import EVENT, METHOD_RESPONSE
+import pyVideoSDK.consts as C
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import screeninfo
+import time
+from enum import Enum
+import wstyle
+from datetime import datetime
+
+# ========================
+# Configuration
+# ========================
+ROOM_IP: str = "127.0.0.1"
+ROOM_PORT: int = 2233
+PIN: str = "123"
+
+# ========================
+
+BUTTON_WIDTH: int = 160
+BUTTON_HEIGHT: int = 90
+INDENT: int = 100
+
+MONITOR: int = 0
+CALL_ID: str = "azobov@team.trueconf.com"
+IMAGE_BUTTON: str = "call.png"
+
+sdk = pyVideoSDK.open_session(ip = ROOM_IP, port = ROOM_PORT, pin = PIN, debug = True)
+methods = Methods(sdk)
+
+class KioskButton(QWidget):
+    def __init__(self, monitor: int):
+        super().__init__()
+
+        self.layout = None
+        self.room = None
+        self.displayName = ""
+        self.buttonCall = None
+        self.buttonCallTag = False
+        if monitor >= len(screeninfo.get_monitors()) or monitor < 0:
+            monitor = 0
+        self.monitor = screeninfo.get_monitors()[monitor]
+        self.state = 0
+        self.initUI()
+
+    def initUI(self):
+        # Setting layout
+        self.layout = QVBoxLayout(self)
+        # Position
+        self.setPosition()
+
+        # Frameless window
+        self.setWindowFlags(Qt.WindowStaysOnTopHint 
+            | Qt.X11BypassWindowManagerHint | Qt.FramelessWindowHint | Qt.Dialog)
+        self.setAttribute(Qt.WA_NoSystemBackground)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        # Show
+        self.show()
+        # connectToRoom
+        self.connectToRoom()
+        # Button
+        self.buttonCall = self.addButton(style = wstyle.STYLE_GRAY_BUTTON, icon = IMAGE_BUTTON)
+        # flashing timer
+        self.timer = QTimer(self, interval = 500)
+        self.timer.timeout.connect(self.flashing)
+        self.timer.start()
+
+    def setPosition(self):
+        # calc window size & position
+        width: int = 200
+        height: int = 120
+        leftCoord: int = self.monitor.x + self.monitor.width - width - INDENT
+        topCoord: int = self.monitor.y + self.monitor.height - height - INDENT
+
+        # set position
+        print(f'{leftCoord=}, {topCoord=}, {width=}, {height=}')
+        self.setGeometry(leftCoord, topCoord, width, height)
+
+    def flashing(self):
+        if self.state == 3:
+            if self.buttonCallTag:
+                self.buttonCall.setStyleSheet(wstyle.STYLE_GREEN_BUTTON_FLASH)
+            else:
+                self.buttonCall.setStyleSheet(wstyle.STYLE_GREEN_BUTTON)
+
+        self.buttonCallTag = not self.buttonCallTag
+
+    def addButton(self, style: str, icon: str):
+        button = QPushButton()
+        button.setStyleSheet(style)
+        button.setFixedWidth(BUTTON_WIDTH)
+        button.setFixedHeight(BUTTON_HEIGHT)
+        button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        self.layout.addWidget(button)
+        self.layout.setAlignment(button, Qt.AlignRight | Qt.AlignBottom)
+
+        if icon:
+            button.setIcon(QIcon(icon))
+            sizeIcon: int = 48
+            button.setIconSize(QSize(sizeIcon, sizeIcon))
+
+        button.clicked.connect(self.on_click_call)
+        return button
+
+    def connectToRoom(self):
+        pass
+
+    def on_state_change(self, response):
+        try:
+            self.state = response["appState"]
+            print(f'onChangeState( {self.state} )')
+
+            # Button color
+            if self.state == 3:
+                style = wstyle.STYLE_GREEN_BUTTON 
+            elif self.state in [4, 5]:
+                style = wstyle.STYLE_RED_BUTTON
+            else:
+                style = wstyle.STYLE_GRAY_BUTTON
+            self.buttonCall.setStyleSheet(style)
+
+            if self.state in [4, 5]:
+                methods.showMainWindow(True, stayOnTop = False)  # Maximized
+            else:
+                methods.showMainWindow(False) # Minimized, Hiden
+        except Exception as e:
+            print(f'Exception "{e.__class__.__name__}" in {__file__}:{sys._getframe().f_lineno}: {e}')
+
+    @pyqtSlot()
+    def on_click_call(self):
+        self.displayName = self.sender().text
+        if self.state == 3:
+            methods.call(CALL_ID)
+        else:
+            methods.hangUp(True)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    call_button = KioskButton(MONITOR)
+    
+    # Add handlers
+    sdk.add_handler(EVENT[C.EV_appStateChanged], call_button.on_state_change)
+    sdk.add_handler(METHOD_RESPONSE[C.M_getAppState], call_button.on_state_change)
+    # Request the current state
+    methods.getAppState()
+
+    sys.exit(app.exec_())
---
+++ wstyle.py
@@ -0,0 +1,131 @@
+STYLE_DEFAULT: str = '''background-color: #0097a7;
+                    text-align: left;
+                    padding: 20px;
+                    color: white;
+                    font: bold 50px;'''
+
+STYLE_TRANSPARENT: str = '''background-color: transparent;'''
+
+STYLE_PASS_CARD_FIRST: str = '''background-color: white;
+                    image: url("cyanCard.svg");
+                    border-top-left-radius: 20px;
+                    border-top-right-radius: 20px;
+                    padding: 20px;'''
+
+STYLE_PASS_CARD_NEW: str = '''background-color: white;
+                    image: url("greenCard.svg");
+                    border-top-left-radius: 20px;
+                    border-top-right-radius: 20px;
+                    padding: 20px;'''
+
+STYLE_PASS_CARD_DELETE: str = '''background-color: white;
+                    image: url("redCard.svg");
+                    border-top-left-radius: 20px;
+                    border-top-right-radius: 20px;
+                    padding: 20px;'''
+
+STYLE_PASS_CARD_TEXT: str = '''background-color: white;
+                    border-bottom-left-radius: 20px;
+                    border-bottom-right-radius: 20px;
+                    padding: 20px;
+                    color: black;
+                    font: bold 24px;'''
+
+STYLE_RED_BUTTON: str = '''background-color: red;
+                    border-radius: 6px;
+                    padding: 20px;
+                    color: white;
+                    font: bold 28px'''
+
+STYLE_ADDRESS_BOOK: str = '''background-color: white;
+                    border-radius: 20px;
+                    padding: 20px;
+                    color: black;
+                    text-align: left;
+                    font: bold 28px'''
+
+STYLE_EXIT_AND_RETURN_BUTTONS: str = '''background-color: #7A000000;
+                    border-radius: 20px;
+                    padding: 20px;
+                    color: white;
+                    text-align: center;
+                    font: bold 28px'''
+
+STYLE_DEFAULT_INFO: str = '''background-color: white;
+                    text-align: left;
+                    padding: 20px;
+                    color: #0097a7;
+                    font: bold 50px;'''
+
+STYLE_CONFIRM_IMAGE: str = '''background-color: qlineargradient(x1:0, y1:1, x2:1, y2:1, stop:0 #A0CFA1, stop:1 #4CB378);
+                    border-top-left-radius: 20px;
+                    border-top-right-radius: 20px;
+                    padding: 20px;'''
+
+STYLE_CONFIRM_TEXT: str = '''background-color: qlineargradient(x1:0, y1:1, x2:1, y2:1, stop:0 #A0CFA1, stop:1 #4CB378);
+                    border-radius: 20px;
+                    padding: 20px;
+                    color: white;
+                    font: bold 32px;'''
+
+STYLE_INVALID_TEXT: str = '''background-color: white;
+                    border-radius: 20px;
+                    padding: 20px;
+                    color: red;
+                    font: bold 32px;'''
+
+STYLE_CALLING_TEXT: str = '''background-color: transparent;
+                    color: white;
+                    text-align: center;
+                    font: bold 36px;'''
+
+STYLE_RED_INFO: str = '''background-color: white;
+                    text-align: center;
+                    padding: 20px;
+                    border-radius: 6px;
+                    color: red;
+                    font: bold 20px;'''
+        
+STYLE_BUTTON_ON_LIST: str = '''background-color: red;
+                    text-align: left;
+                    border-radius: 6px;
+                    padding-left: 20px;
+                    color: white;
+                    font: bold 20px;'''
+
+STYLE_LIST_UP_TEXT: str = '''text-align: left;
+                    color: black;
+                    font: bold 20px;'''
+
+STYLE_LIST_DOWN_TEXT: str = '''text-align: left;
+                    color: black;
+                    font: 18px;'''
+
+STYLE_LIST_IMAGE: str = '''border-radius: 6px;'''
+
+STYLE_CARD_WIDGET: str = '''background-color: white;
+                    border-radius: 20px;'''
+
+STYLE_RED_BUTTON: str = '''background-color: red;
+                    border-radius: 6px;
+                    padding: 20px;
+                    color: white;
+                    font: bold 28px'''
+
+STYLE_GREEN_BUTTON: str = '''background-color: green;
+                    border-radius: 6px;
+                    padding: 20px;
+                    color: white;
+                    font: bold 28px'''
+
+STYLE_GREEN_BUTTON_FLASH: str = '''background-color: #00EF00;
+                    border-radius: 6px;
+                    padding: 20px;
+                    color: white;
+                    font: bold 28px'''
+
+STYLE_GRAY_BUTTON: str = '''background-color: gray;
+                    border-radius: 6px;
+                    padding: 20px;
+                    color: white;
+                    font: bold 28px'''
